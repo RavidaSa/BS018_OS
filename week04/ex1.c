de10b168d8952c0d09d953d5a623751529d9cca3
Ravi <ravidasaitova@gmail.com>
2019-09-12 17:46:54 +0300

    Update ex1.c

--- week04/ex1.c
+++ week04/ex1.c
@@ -12,6 +12,10 @@ else
     printf("Hello from parent [%d - %d]\n", getpid(), n);
 
 return 0;
+/* In the above code, a child process is created, fork() 
+returns 0 in the child process and positive integer to the parent process.
+Here, two outputs are possible because the parent process and child process are running concurrently. 
+So we don’t know if OS first give control to which process a parent process or a child process.*/
 }
 
 
