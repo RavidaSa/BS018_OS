2bc76d39c7aab5b27b54341d3200420132c4b48f
Ravi <ravidasaitova@gmail.com>
2019-09-12 18:14:52 +0300

    Update ex2.c

--- week04/ex2.c
+++ week04/ex2.c
@@ -9,7 +9,8 @@ fork();
 sleep(5);
 
 }
-/* 8 processes are created in general. But at each iteration of the for loop current parent and correspondent child create 2 more processes*/
+/* 8 processes are created in general. But at each iteration of the for loop current parent and 
+correspondent child create 2 more processes*/
 
 return 0;
 }
