8df73ccb6dc98777eb676c1bda5eec567f261014
Ravi <ravidasaitova@gmail.com>
2019-10-03 21:54:15 +0300

    Create ex5.c

---
+++ week07/ex5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char **s;
+    char *temp;
+    char foo[] = "Hello World";
+    temp = foo;
+    s = &temp;
+    printf("s is %s\n", *s);
+    *s[0] = foo[0];
+    printf("s[0] is %c\n", *s[0]);
+
+    return(0);
+}
